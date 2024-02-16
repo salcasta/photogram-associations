6621719ae8104f5e1c92f7e24468bf7d0029807b
Salvador Castaneda <57605337+salcasta@users.noreply.github.com>
2024-02-16 18:42:24 +0000

    associations complete

--- app/models/like.rb
+++ app/models/like.rb
@@ -22,7 +22,11 @@ class Like < ApplicationRecord
 
   # Like#fan: returns a row from the users table associated to this like by the fan_id column
 
+  belongs_to(:fan, class_name: "User", foreign_key: "fan_id")
+
   # Like#photo: returns a row from the photo table associated to this like by the photo_id column
+
+  belongs_to(:photo)
 
   def fan
     my_fan_id = self.fan_id
